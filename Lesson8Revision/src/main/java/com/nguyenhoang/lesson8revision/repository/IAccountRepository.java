4550815dd62b06595d1877e78460dc92793d1522
Nguyen Hoang <metmoilamroiday4@gmail.com>
2023-02-23 19:42:13 +0700

    Delete from job repository

--- Lesson8Revision/src/main/java/com/nguyenhoang/lesson8revision/repository/IAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nguyenhoang.lesson8revision.repository;
-
-import com.nguyenhoang.lesson8revision.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-//JpaSpecificationExecutor: thêm mapping cho page
-public interface IAccountRepository extends JpaRepository<Account,Integer>, JpaSpecificationExecutor<Account> {
-}
--- Lesson8Revision/src/main/java/com/nguyenhoang/lesson8revision/repository/IDepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nguyenhoang.lesson8revision.repository;
-
-import com.nguyenhoang.lesson8revision.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-//jpaSpecificationExecutor: mapping mà có paging
-public interface IDepartmentRepository extends JpaRepository<Department,Integer>, JpaSpecificationExecutor<Department> {
-}
