d519f7bd70932afd5f11938e2fbd716d2f07bbf9
Nguyen Hoang <metmoilamroiday4@gmail.com>
2023-03-17 17:37:57 +0700

    delete not important file

--- AirportManagement_Frontend/js/home/home.js
+++
@@ -1,5 +0,0 @@
-function viewHomepage() {
-    $(".main").load("./home/homePage.html", function () {
-        document.getElementById("line-2").innerHTML = 'Hello ' + storage.getItem("LAST_NAME") + "!";
-    });
-}
